1d72bc02fe6e102dde9058a15c5945c5965d2568
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2022-01-24 20:47:19 -0600

    harry styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>HI BEB</h1>
+      <img src="https://www.rollingstone.com/wp-content/uploads/2019/08/20190723_Rolling_Stone_Harry_Styles_Rocks_0119_03_ext_RGB-LEAD-NEW.jpg" alt="harry"/>
     </div>
   );
 }

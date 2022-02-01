fef8c2bc738442244b46e9779d56207785121ec7
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2022-02-01 16:44:32 -0600

    made a header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import {Route, Routes} from "react-router-dom"
-import AddFoodForm from "../src/foodOptions/AddFood"
-import EditFoodForm from "../src/foodOptions/EditFood"
-import HomePage from "./foodOptions/Homepage.js"
+import AddFoodForm from "./components/AddFood"
+import EditFoodForm from "./components/EditFood"
+import HomePage from "./components/Homepage.js"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/add" element={<AddFoodForm />} />
-        <Route path="/food"  />
-        <Route path="/food/:id" />
-        <Route path="/food/edit/:id" element={<EditFoodForm />} />
-        
-      </Routes>
+      <Header>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddFoodForm />} />
+          <Route path="/food"  />
+          <Route path="/food/:id" />
+          <Route path="/food/edit/:id" element={<EditFoodForm />} />
+        </Routes>
+      </Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <NavLink to="/">
+                    Home
+                </NavLink>
+                <NavLink to="/add">
+                    Add Food
+                </NavLink>
+                <NavLink to="food">
+                    See Food
+                </NavLink>
+            </nav>
+        </header>
+    )
+}

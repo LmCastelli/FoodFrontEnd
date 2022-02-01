16fd32108058d7254b5593cdc16ac5d374748920
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2022-02-01 15:59:31 -0600

    Trying to see if homepage can load

--- src/foodOptions/Homepage.js
+++ src/foodOptions/Homepage.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Homepage = () => {
-
+export default function Homepage()  {
 
     return (
         <div>
             <h1>HOMEPAGE</h1>
+            <h1>I wish this would work actually</h1>
         </div>
     )
-}
+};
 
-export default Homepage;
